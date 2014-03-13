9966cbbb8bc7f1994119b916932d52791e7431db
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-13 15:10:19 +0100

    spec corrected

--- spec/omniauth/strategies/vphticket_spec.rb
+++ spec/omniauth/strategies/vphticket_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe OmniAuth::Strategies::VphTicket do
+describe OmniAuth::Strategies::Vphticket do
 
   class VphticketProvider < OmniAuth::Strategies::Vphticket; end
 
